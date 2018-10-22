c6d829ff5e858c1c51eee9073ada3f064a7b1a91
PrimeAndrew <raparicio71@gmail.com>
2018-10-22 10:23:49 -0400

    migrate: create_zone_table

---
+++ taller2018/database/migrations/2018_10_22_052420_create_zone_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateZoneTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zone', function (Blueprint $table) {
+            $table->increments('id_zone');
+            $table->string('zone',100);
+            $table->string('city',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zone');
+    }
+}
